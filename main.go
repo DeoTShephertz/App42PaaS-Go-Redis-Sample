c3c1dc854d1803ceb3f5ec62abecf8d7b9e8b65e
PravinKShepHertz <pravin.kumar@shephertz.co.in>
2014-07-02 16:39:55 +0530

    changed field name

--- main.go
+++ main.go
@@ -2,37 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/astaxie/goredis"
+	"github.com/hoisie/redis"
 	"net/http"
+	"html/template"
 	"log"
-	"text/template"
 )
-
-type User struct {
-	UserName string
-}
 
 var (
-	client goredis.Client
+	client redis.Client
 )
 
-func setupDB(){
-	client.Addr = "127.0.0.1:6379"
-	// client, err := DialURL("tcp://auth:password@127.0.0.1:6379/0?timeout=10s&maxidle=1")
+type Profile struct {
+	User string
 }
 
-func indexHanlder(w http.ResponseWriter, r *http.Request){
-	fmt.Println("In index handler!")
-    username,_ := client.Lrange("username", 0, 10000)
-	users := []User{}
-	for _, v := range username {
-		user := User{}
-		user.UserName = string(v)
-		users = append(users, user)
-	}
-	t := template.New("index.html")
-	t.ParseFiles("templates/index.html")
-	t.Execute(w, users)
+func setupDB() {
+    client.Addr = "VM IP:VM PORT"
+    client.Db = 0
+    client.Password = "PASSWORD"
 }
 
 func newHandler(w http.ResponseWriter, r *http.Request) {
@@ -44,22 +31,43 @@ func newHandler(w http.ResponseWriter, r *http.Request) {
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("In save handler!")
-	username := r.FormValue("username")
- 	client.Rpush("username", []byte(username))
+	user := r.FormValue("user")
+	err := client.Rpush("user", []byte(user))
+
+	fmt.Println("Redis Error:", err)
 	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func indexHanlder(w http.ResponseWriter, r *http.Request){
+	fmt.Println("In get data.")
+	username, _ := client.Lrange("user", 0, 10000)
+	fmt.Println("User--->", username)
+
+	profiles := []Profile{}
+	for i, v := range username {
+		println("Name", i,":",string(v))
+
+		profile := Profile{}
+		profile.User = string(v)
+		profiles = append(profiles, profile)
+	}
+
+	t := template.New("index.html")
+	t.ParseFiles("templates/index.html")
+	fmt.Println("Final user================>", profiles)
+	t.Execute(w, profiles)
 }
 
 func main() {
 	setupDB()
-	fmt.Println("Redis db connected: ", client)
-
+	fmt.Println("Client: ", client)
 	http.HandleFunc("/", indexHanlder)
-	http.HandleFunc("/new/", newHandler)
 	http.HandleFunc("/save/", saveHandler)
-	fmt.Println("Listening on port 3000....")
+	http.HandleFunc("/new/", newHandler)
+
 	http.Handle("/public/css/", http.StripPrefix("/public/css/", http.FileServer(http.Dir("public/css"))))
 	http.Handle("/public/images/", http.StripPrefix("/public/images/", http.FileServer(http.Dir("public/images"))))
 	if err := http.ListenAndServe("0.0.0.0:3000", nil); err != nil {
-		log.Fatalf("Error in listening:", err)	
+		log.Fatalf("Error in listening:", err)
 	}
 }
